3d761ed155fcf3ca8411bce4364ba7b97899f7b3
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-06-23 15:29:49 -0600

    Added basic visual nodes to the ends of the line. They also change color as you hover over them.

--- kruskalAlg/Assets/Scripts/DrawLine.cs
+++ kruskalAlg/Assets/Scripts/DrawLine.cs
@@ -6,14 +6,14 @@ public class DrawLine : MonoBehaviour
 {
     // Line object references
     public GameObject linePrefab;
-    public GameObject currLine;
+    private GameObject currLine;
 
     // Components of line
-    public LineRenderer lineRenderer;
-    public EdgeCollider2D edgeCollider;
-    public List<Vector2> fingerPositions;
+    private LineRenderer lineRenderer;
+    private List<GameObject> nodeArr = new List<GameObject>();
+    private List<Vector2> fingerPositions = new List<Vector2>();
 
-    // Whether a line has begun to be drawn or not.
+    // Whether a line has begun to be drawn or not
     private bool lineActive = false;
 
     // Update is called once per frame
@@ -27,22 +27,33 @@ public class DrawLine : MonoBehaviour
     void CreateLine() {
         if (!lineActive) {
             // Instantiate line and get its components
-            currLine = Instantiate(linePrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            currLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
             lineRenderer = currLine.GetComponent<LineRenderer>();
-            edgeCollider = currLine.GetComponent<EdgeCollider2D>();
+            nodeArr.Add(currLine.transform.Find("node1").gameObject);
+            nodeArr.Add(currLine.transform.Find("node2").gameObject);
 
-            // Make current mouse position the starting point for the line. Set lineActive.
+            // Make current mouse position the starting point for the line
             fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             lineRenderer.SetPosition(0, fingerPositions[0]);
             lineRenderer.SetPosition(1, fingerPositions[0]);
-            edgeCollider.points = fingerPositions.ToArray();
+
+            // Position/activate node and set lineActive to true
+            nodeArr[0].transform.position = fingerPositions[0];
+            nodeArr[0].SetActive(true);
+
             lineActive = true;
         } else {
-            // Get second position for the end of the line, set lineActive to false, and clear fingerpositions.
+            // Get second position for the end of the line 
             fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             lineRenderer.SetPosition(1, fingerPositions[1]);
-            edgeCollider.points = fingerPositions.ToArray();
+
+            // Position/activate node
+            nodeArr[1].transform.position = fingerPositions[1];
+            nodeArr[1].SetActive(true);
+
+            // Set lineActive to false, and clear node positions and fingerpositions
             lineActive = false;
+            nodeArr.Clear();
             fingerPositions.Clear();
         }
     }
---
+++ kruskalAlg/Assets/Scripts/MouseDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDetection : MonoBehaviour
+{
+    public bool onNode;
+    private Material nodeColor;
+
+    private void Start() {
+        nodeColor = GetComponent<Renderer>().material;
+    }
+
+    private void OnMouseOver() {
+        onNode = true;
+        nodeColor.SetColor("_Color", Color.red);
+    }
+
+    private void OnMouseExit() {
+        onNode = false;
+        nodeColor.SetColor("_Color", Color.blue);
+    }
+}
