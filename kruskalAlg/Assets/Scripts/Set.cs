fa4a978eb6934956575f965517a1f63c7b867d69
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-06-27 12:12:17 -0600

    Created Set and UnionFind scripts for Kruskal's alg

---
+++ kruskalAlg/Assets/Scripts/Set.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set : MonoBehaviour {
+
+    // Reference and rank of set
+    private string ID;
+    private int rank;
+    private Set parent;
+
+    // Constructor
+    public Set(string newID) {
+        ID = newID;
+        rank = 0;
+        parent = null;
+    }
+
+    public string getID() {
+        return ID;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public Set getParent() {
+        return parent;
+    }
+
+    public void setID(string newID) {
+        ID = newID;
+    }
+
+    public void setRank(int newRank) {
+        rank = newRank;
+    }
+
+    public void setParent(Set newParent) {
+        parent = newParent;
+    }
+}
---
+++ kruskalAlg/Assets/Scripts/UnionFind.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnionFind : MonoBehaviour {
+
+    // Reference to all nodes
+    private List<Set> forest;
+
+    // Constructor
+    public UnionFind() {
+        forest = null;
+    }
+
+    // Creates a new set and adds it to forest
+    public void createSet(string newID) {
+        Set newSet = new Set(newID);
+        forest.Add(newSet);
+    }
+
+    // Finds the parent set which contains set
+    public Set findSet(Set set) {
+        while (set.getParent() != null) { 
+            findSet(set.getParent()); 
+        }
+        return set;   
+    }
+
+    // Combines the two sets if they belong to different sets
+    public void union(Set set1, Set set2) {
+        Set parentSet1 = findSet(set1);
+        Set parentSet2 = findSet(set2);
+
+        // If the parents are not the same, compare ranks and set the lower ranked parent to the higher ranked.
+        if (parentSet1 != parentSet2) {
+            if (parentSet1.getRank() >= parentSet2.getRank()) {
+                parentSet2.setParent(parentSet1);
+                // HERE, SET THE LINE ACTIVES BETWEEN SET1 AND SET2
+            } else {
+                parentSet1.setParent(parentSet2);
+                // HERE, SET THE LINE ACTIVES BETWEEN SET1 AND SET2
+            }
+        }
+    }
+}
